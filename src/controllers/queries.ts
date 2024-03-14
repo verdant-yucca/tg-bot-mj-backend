7fe2b1a8407799eac1da3b049b9ce1f04bd97f78
Valerii Katruk <walerik19@mail.ru>
2024-03-14 13:20:50 +0800

    feat/updateQuery added updateQuery

--- src/controllers/queries.ts
+++ src/controllers/queries.ts
@@ -9,16 +9,47 @@ import { ERROR_NOT_FOUND, ERROR_BED_REQUEST } from '../utils/constants';
 interface BodyParams {
     chatId: string;
     prompt: string;
+    _id?: string;
+    flag?: string;
+    button?: string;
 }
 export const queries = async (req: Request<any, any, BodyParams>, res: Response, next: NextFunction) => {
-    const { chatId, prompt } = req.body;
+    const { chatId, prompt, _id, button, flag } = req.body;
+    const foundQuery = await Query.findOne({ _id });
+    const currentDate = new Date().toISOString();
 
-    Query.create({
-        chatId,
-        prompt,
-    })
-        .then(data => {
-            res.send(data);
+    if (foundQuery) {
+        Query.findOneAndUpdate(
+            { _id },
+            {
+                prompt,
+                currentDate,
+                flag,
+                button
+            },
+        )
+            .then(query => {
+                if (query) {
+                    res.send({ query });
+                } else {
+                    throw new NotFoundError(ERROR_NOT_FOUND.messageUser);
+                }
+            })
+            .catch(err => {
+                if (err.name === 'ValidationError' || err.name === 'CastError') {
+                    next(new BadRequestError(ERROR_BED_REQUEST.message));
+                } else {
+                    next(err);
+                }
+            });
+    } else {
+        Query.create({
+            chatId,
+            prompt,
         })
-        .catch(() => next(new BadRequestError(ERROR_BED_REQUEST.message)));
+            .then(data => {
+                res.send(data);
+            })
+            .catch(() => next(new BadRequestError(ERROR_BED_REQUEST.message)));
+    }
 };
